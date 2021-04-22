c43144fc0dafd4d16e2dbee38097c1f78958d85c
Archit Gupta <archit.gupta@booking.com>
2021-04-22 15:43:16 +0530

    Prepare the project for deployment via Kubernetes

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ function App() {
         <header>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
-          <h1>Fib Calculator</h1>
+          <h1>Fib Calculator version 2</h1>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
